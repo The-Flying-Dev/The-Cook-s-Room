4236ef06ec258d48db3b1e8e774bd49f0676541b
Leandran <41592532+Leandran@users.noreply.github.com>
2021-02-16 03:07:48 +0200

    generated scaffold

---
+++ test/system/recipes_test.rb
@@ -0,0 +1,57 @@
+require "application_system_test_case"
+
+class RecipesTest < ApplicationSystemTestCase
+  setup do
+    @recipe = recipes(:one)
+  end
+
+  test "visiting the index" do
+    visit recipes_url
+    assert_selector "h1", text: "Recipes"
+  end
+
+  test "creating a Recipe" do
+    visit recipes_url
+    click_on "New Recipe"
+
+    fill_in "Cooking time", with: @recipe.cooking_time
+    fill_in "Cuisine", with: @recipe.cuisine_id
+    fill_in "Difficulty level", with: @recipe.difficulty_level
+    fill_in "Food preference", with: @recipe.food_preference_id
+    fill_in "Food type", with: @recipe.food_type_id
+    fill_in "Ingredients", with: @recipe.ingredients
+    fill_in "Procedure", with: @recipe.procedure
+    fill_in "Title", with: @recipe.title
+    click_on "Create Recipe"
+
+    assert_text "Recipe was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Recipe" do
+    visit recipes_url
+    click_on "Edit", match: :first
+
+    fill_in "Cooking time", with: @recipe.cooking_time
+    fill_in "Cuisine", with: @recipe.cuisine_id
+    fill_in "Difficulty level", with: @recipe.difficulty_level
+    fill_in "Food preference", with: @recipe.food_preference_id
+    fill_in "Food type", with: @recipe.food_type_id
+    fill_in "Ingredients", with: @recipe.ingredients
+    fill_in "Procedure", with: @recipe.procedure
+    fill_in "Title", with: @recipe.title
+    click_on "Update Recipe"
+
+    assert_text "Recipe was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Recipe" do
+    visit recipes_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Recipe was successfully destroyed"
+  end
+end
