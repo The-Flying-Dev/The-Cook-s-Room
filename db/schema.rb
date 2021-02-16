45c6bda342bb8b6cb7dc98c50688f721e322ffb5
Leandran <41592532+Leandran@users.noreply.github.com>
2021-02-16 02:51:58 +0200

    added 2 more models, food_preference.rb and cuisine.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_001337) do
+ActiveRecord::Schema.define(version: 2021_02_16_004916) do
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "foo_preferences", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "food_preferences", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "food_types", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 #default data
 food_types = ["Curry", "Dessert", "Sides", "Breakfast"]
 food_types.each{|d| FoodType.where(:name => d).first_or_create} #checks for record before adding it
+
+food_preferences = ["Vegetarian", "Vegan", "Meat", "Dairy"]
+food_preferences.each {|d| FoodPreference.where(:name => d).first_or_create}
+
+cuisines = ["Italian", "Mexican", "indian", "Chinese"]
+cuisines.each {|d| Cuisine.where(:name => d).first_or_create}
