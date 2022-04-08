8c45d5819bffeb211e4e11236ff0e1b84a94ba6f
Leandran <41592532+Leandran@users.noreply.github.com>
2022-04-08 11:12:47 +0200

    Change Recipe view and seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_071058) do
+ActiveRecord::Schema.define(version: 2021_02_16_112936) do
 
   create_table "cuisines", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "foo_preferences", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -47,8 +41,6 @@ ActiveRecord::Schema.define(version: 2022_04_08_071058) do
     t.text "procedure"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 #default data for options to be sent to the database from the form
-food_types = ["Dinner", "Lunch", "Dessert", "Sides", "Breakfast"]
+food_types = ["Breakfast", "Lunch", "Dinner", "Dessert", "Sides", "Snacks"]
 food_types.each{|d| FoodType.where(:name => d).first_or_create} #checks for record before adding it
 
-food_preferences = ["Vegetarian", "Vegan", "Meat", "Dairy", "Candy"]
+food_preferences = ["Vegetarian", "Vegan", "Seafood", "Meat", "Dairy", "Fruits" "Sweets"]
 food_preferences.each {|d| FoodPreference.where(:name => d).first_or_create}
 
-cuisines = ["Italian", "Mexican", "indian", "Chinese"]
+cuisines = ["Italian", "Mexican", "indian", "Chinese", "French", "Greek", "Spanish", "Mediterranean", "Thai"]
 cuisines.each {|d| Cuisine.where(:name => d).first_or_create}
